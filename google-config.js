28d119470c7febcc9f8f4c28a9772f4df4242505
rulymauzi-byte <rulymauzi@gmail.com>
2026-09-13 13:20:22 -0600

    Add files via upload

--- google-config.js
+++ google-config.js
@@ -1,2 +1,2 @@
 /* ID público de MAUZI NOTE. Cada persona autoriza SU PROPIO Drive. */
-window.MAUZI_GOOGLE_CLIENT_ID = "565407871919-noe0ldgi2bdtja1ob8oiq31uqbq13rg1.apps.googleusercontent.com";
+window.MAUZI_GOOGLE_CLIENT_ID = "516673485096-rdoo0mn62ehurum4oqkn16ogh0rcfa5j.apps.googleusercontent.com";
